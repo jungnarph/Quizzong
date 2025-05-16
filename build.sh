f6cb701e008b926c6d4a3597c0ac7c8ec8db608f
Exulten <166355329+TIPmigs@users.noreply.github.com>
2025-05-16 13:10:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser *only if not exists*
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = '$DJANGO_SUPERUSER_USERNAME'
+email = '$DJANGO_SUPERUSER_EMAIL'
+password = '$DJANGO_SUPERUSER_PASSWORD'
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+END
